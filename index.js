90b4de91da0dabe812c09fe4c0595cbf82a8be8e
Dunia Nasr <dunia.nasr777@gmail.com>
2020-05-14 19:48:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let newHeader = document.querySelector("h1#victory");
+newHeader.nodeName='H1';
+
+let newHeader = document.querySelector("h1#victory");
+newHeader.id='victory';
+
+let newHeader = document.querySelector("h1#victory");
+newHeader.innerHTML="is the champion";
